b236536a08c13d5f6389451ced1fe3d21baceacf
correa <4correavitor@gmail.com>
2023-11-18 21:00:18 -0300

    Working

--- lista5.m
+++ lista5.m
@@ -39,7 +39,18 @@ function polynomial_reg(points, order)
 
   plotPoints(points);
   plotFunc(coefs, points(1, 1) - 2, points(1, end) + 2, 0.1);
+  plot_estimate_point(coefs, 6)
 
+  legend;
+
+  sr = get_sr(points, coefs);
+  st = get_st(points);
+
+  r_2 = (st - sr)/ st;
+  r = sqrt(r_2);
+
+  printf("r^2= %.16f\n", r_2);
+  printf("r= %.16f", r);
 endfunction
 
 function coefs = get_coeficients(matrix)
@@ -57,7 +68,7 @@ function plotFunc(coefs, xMin, xMax, step)
     yValues(i) = functionOfX(coefs, xValues(i));
   endfor
 
-  plot(xValues, yValues);
+  plot(xValues, yValues, 'DisplayName', 'f(x) da reg polinomial');
 
 endfunction
 
@@ -71,12 +82,45 @@ function y = functionOfX(coefs, x)
 endfunction
 
 function plotPoints(points)
+  pointsToPlot = zeros(2, length(points(1, :)));
   for i=1: 1: length(points(1, :))
     currentPoint = points(:, i);
     x = currentPoint(1, 1);
     y = currentPoint(2, 1);
 
-    scatter(x, y, 'filled', 'MarkerFaceColor', [0.5 , 0.5, 0.5]);
-    hold on;
+    pointsToPlot(:, i) = [x; y];
+
   endfor
+
+  scatter(pointsToPlot(1, :), pointsToPlot(2, :), 'filled', 'MarkerFaceColor', [0.5 , 0.5, 0.5], 'DisplayName', 'Pontos do enunciado');
+  hold on;
+
+endfunction
+
+function plot_estimate_point(coefs, xValue)
+  y = functionOfX(coefs, xValue);
+  scatter(xValue, y, 'filled', 'DisplayName', 'Ponto Estimado em x=6', 'MarkerFaceColor', 'r');
+  hold on;
+endfunction
+
+function sr = get_sr(points, coefs)
+  currentSum = 0;
+  xValues = points(1, :);
+  yValues = points(2, :);
+  for i=1: length(xValues)
+    currentSum = currentSum + (yValues(i) - functionOfX(coefs, xValues(i)))^2;
+  endfor
+
+  sr = currentSum;
+
+endfunction
+
+function st = get_st(points)
+  currentSum = 0;
+  yValues = points(2, :);
+  for i=1: length(yValues)
+    currentSum = currentSum + (yValues(i) - mean(yValues))^2;
+  endfor
+
+  st = currentSum;
 endfunction
